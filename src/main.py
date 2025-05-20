5772efe833955e3934a842e045f565c3727eeee6
Robin SHAMSNEJAD <rsham@free.fr>
2025-05-20 03:12:31 +0200

    Add time code

--- src/main.py
+++ src/main.py
@@ -3,12 +3,19 @@ from customprint import customprint
 customprint("Importing libraries works!")
 
 # https://docs.micropython.org/en/latest/esp8266/tutorial/neopixel.html
-import machine, neopixel, time
+import machine, neopixel, time, ntptime
 
 NUM_LEDS = 24
 GPIO_DIN = 2
 
 np = neopixel.NeoPixel(machine.Pin(GPIO_DIN), NUM_LEDS)
+
+try:
+    ntptime.settime()
+except:
+    print("Unable to retrieve NTP time!")
+
+print(f"Current local time: {time.localtime()}")
 
 for i in range(NUM_LEDS-1, -1, -1):
     # print(f"Lighting up LED {i}")
