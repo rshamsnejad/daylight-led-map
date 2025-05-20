8aab45696d03240c9b20c9b9b9a2e69f7e48c8c7
Robin SHAMSNEJAD <rsham@free.fr>
2025-05-20 13:36:26 +0200

    Refactor

--- src/main.py
+++ src/main.py
@@ -3,10 +3,14 @@ from customprint import customprint
 customprint("Importing libraries works!")
 
 # https://docs.micropython.org/en/latest/esp8266/tutorial/neopixel.html
-import machine, neopixel, time, ntptime
+import machine
+import neopixel
+import time
+import ntptime
 
 NUM_LEDS = 24
 GPIO_DIN = 2
+SUNLIGHT_RGB = (244, 233, 155)
 
 np = neopixel.NeoPixel(machine.Pin(GPIO_DIN), NUM_LEDS)
 
@@ -21,6 +25,6 @@ print(f"Current local time: {time.localtime()}")
 
 for i in range(NUM_LEDS-1, -1, -1):
     # print(f"Lighting up LED {i}")
-    np[i] = (244, 233, 155)
+    np[i] = SUNLIGHT_RGB
     np.write()
     time.sleep(0.1)
