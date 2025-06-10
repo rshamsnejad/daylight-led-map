a616cbe2fc5a27b4489e83364db6de279bd10ab3
Robin SHAMSNEJAD <rsham@free.fr>
2025-06-10 23:26:30 +0200

    Improve testing program

--- src/matrix_test.py
+++ src/matrix_test.py
@@ -2,9 +2,26 @@ import machine
 import neopixel
 import time
 from neopixel_matrix import xy_to_index
+from sys import argv
 
-NUM_LEDS = 512
+MODE = "hardware"
+# MODE = "simulation"
+WIDTH = 32
+HEIGHT = 16
+SQUARE_SPLIT = True
+NUM_LEDS = WIDTH * HEIGHT
 GPIO_DIN = 2
+
+if MODE == "hardware":
+    zigzag = True
+    flip_x = True
+    # flip_x = False
+    intensity = 10
+else:
+    zigzag = False
+    flip_x = False
+    # flip_x = True
+    intensity = 255
 
 np = neopixel.NeoPixel(machine.Pin(GPIO_DIN), NUM_LEDS)
 
@@ -12,10 +29,32 @@ np.fill((0, 0, 0))
 np.write()
 
 for i in range(NUM_LEDS):
-    np[i] = (0, 0, 255)
+    np[i] = (0, 0, intensity)
+    # time.sleep(0.1)
+    # np.write()
 np.write()
 
-np[xy_to_index(0, 0, width=32, height=16, zigzag=False, square_split_x=True)] = (255, 0, 0)
-np[xy_to_index(2, 1, width=32, height=16, zigzag=False, square_split_x=True)] = (255, 0, 0)
-np[xy_to_index(16, 5, width=32, height=16, zigzag=False, square_split_x=True)] = (255, 0, 0)
-np.write()
+red_dots = [
+    (0, 0),
+    (0, 1),
+    (2, 1),
+    (15, 15),
+    (16, 5),
+    (31, 15)
+]
+
+for dot in red_dots:
+    index = xy_to_index(
+            dot[0], dot[1],
+            width=WIDTH,
+            height=HEIGHT,
+            zigzag=zigzag,
+            flip_x=flip_x,
+            square_split_x=SQUARE_SPLIT
+        )
+
+    print(f"index = {index}")
+
+    np[index] = (intensity, 0, 0)
+
+    np.write()
