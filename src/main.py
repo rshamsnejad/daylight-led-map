afda7d259c534581bc99f860ce30ef751162fe10
Robin SHAMSNEJAD <rsham@free.fr>
2025-06-14 15:20:00 +0200

    Add simulation-hardware conditions

--- src/main.py
+++ src/main.py
@@ -6,11 +6,27 @@ import ntptime
 from wifi import connect_wifi
 from neopixel_matrix import xy_to_index, xy_to_lonlat
 from sun_math import sun_position, is_daytime
-from secrets.home_wifi import home_wifi
+
+# MODE = "hardware"
+MODE = "simulation"
+
+if MODE == "hardware":
+    from secrets.home_wifi import home_wifi
+    SSID = home_wifi['ssid']
+    PASSWORD = home_wifi['password']
+    ZIGZAG = True
+    FLIP_X = True
+    FLIP_Y = True
+else:
+    SSID = 'Wokwi-GUEST'
+    PASSWORD = ''
+    ZIGZAG = False
+    FLIP_X = False
+    FLIP_Y = False
+
 
 try:
-    # connect_wifi('Wokwi-GUEST', '')
-    connect_wifi(home_wifi['ssid'], home_wifi['password'])
+    connect_wifi(SSID, PASSWORD)
 except Exception as e:
     print("Unable to connect to internet!")
     print(e)
@@ -26,10 +42,12 @@ HEIGHT = 16
 NUM_LEDS = WIDTH * HEIGHT
 GPIO_DIN = 2
 
-# DAY =  	(255,249,121)
-# NIGHT = (81,72,178)
-DAY   = (183, 177, 49)
-NIGHT = (9, 0, 106)
+if MODE == "hardware":
+    DAY   = (183, 177, 49)
+    NIGHT = (9, 0, 106)
+else:
+    DAY =  	(255,249,121)
+    NIGHT = (81,72,178)
 
 # Get current UTC timestamp
 utc_now = time.time()
@@ -43,7 +61,7 @@ np = neopixel.NeoPixel(machine.Pin(GPIO_DIN), NUM_LEDS)
 for y in range(HEIGHT):
     for x in range(WIDTH):
         lon, lat = xy_to_lonlat(x, y, width=WIDTH, height=HEIGHT)
-        index = xy_to_index(x, y, zigzag=True, flip_x=True, flip_y=True, square_split_x=True)
+        index = xy_to_index(x, y, zigzag=ZIGZAG, flip_x=FLIP_X, flip_y=FLIP_Y, square_split_x=True)
         
         if is_daytime(lon, math.radians(lat), delta, lambda_s):
             np[index] = DAY
